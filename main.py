ebc25cc3462ab5bf009ffd3182961e5ffdc1b5d7
joecos050628kun@gmail.com <130775789+JoecosB@users.noreply.github.com>
2024-03-13 21:01:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import json
 import botpy
 from botpy.message import Message
 
-APPID = "102096507"
-SECRET = "yV2Z6dAiGoMuS0Y7gFoNwV5fFpPzZAlM"
+# 读取机器人的id和访问密钥
+with open("secret_info.json") as info_file:
+    secret = json.load(info_file)
 
+# 机器人类
 class MyClient(botpy.Client):
     async def on_at_message_create(self, message: Message):
         await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
 
+
 intents = botpy.Intents(public_guild_messages=True) 
 client = MyClient(intents=intents)
-client.run(appid=APPID, secret=SECRET)
+
+
+client.run(appid=secret["APPID"], secret=secret["SECRET"])
